00e4dbf46df1b0cd5b63f2bf502c97723b3ed513
gaofei <gaofei1207@gmail.com>
2017-06-05 14:54:15 +0800

    git teste1

--- MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
@@ -45,22 +45,7 @@ public class MainActivity extends BaseAct {
     }
 
     private void initView2() {
-        mButton = (ImageView) findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SkipAct.class);
-                startActivity(intent);
-            }
-        });
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa2","dddddd");
-        Log.d("test aaa","dddddd123");
+        Log.d("","");
     }
 
 
