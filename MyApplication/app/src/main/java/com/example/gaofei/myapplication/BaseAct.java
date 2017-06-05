19e87a6f440f5eee0170e263c1384f7442f70e6c
gaofei <gaofei1207@gmail.com>
2017-06-05 12:40:20 +0800

    git test

--- MyApplication/app/src/main/java/com/example/gaofei/myapplication/BaseAct.java
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/BaseAct.java
@@ -1,5 +1,8 @@
 package com.example.gaofei.myapplication;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +10,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BaseAct extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
