7f95c2c09d7cd322917903cf742a910f812ec153
gaofei <gaofei1207@gmail.com>
2017-06-05 15:07:25 +0800

    git a4

--- MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends BaseAct {
         Log.d("test aaa","a1");
         Log.d("test aaa","a2");
         Log.d("test aaa","a3");
+        Log.d("test aaa","a4");
     }
 
 
