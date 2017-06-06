a1d075deb7db2e96877e2449821b8599b55734ec
gaofei <gaofei1207@gmail.com>
2017-06-06 17:00:50 +0800

    git  master rebase1

--- MyApplication/app/src/main/java/com/example/gaofei/myapplication/BaseAct.java
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/BaseAct.java
@@ -12,8 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseAct extends AppCompatActivity{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+        super.onCreate(savedInstanceState, persistentState);}
+
 }
