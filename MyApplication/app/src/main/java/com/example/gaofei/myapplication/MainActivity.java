8cfec621536eadfcea9734167c5de8b9309c1e8b
gaofei <gaofei1207@gmail.com>
2017-06-11 12:08:02 +0800

    revert to 3

--- MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/MainActivity.java
@@ -38,8 +38,5 @@ public class MainActivity extends BaseAct {
         Log.d("master 11","1");
         Log.d("master 11","2");
         Log.d("master 11","3");
-        Log.d("master 11","4");
-        Log.d("master 11","5");
-        Log.d("master 11","6");
     }
 }
