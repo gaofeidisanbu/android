89a90017b9fbfda383fd5d19f92309ced53a2093
gaofei <gf>
2017-10-31 14:17:46 +0800

    application2 1

---
+++ MyApplication/app/src/main/java/com/example/gaofei/myapplication/Application2.java
@@ -0,0 +1,13 @@
+package com.example.gaofei.myapplication;
+
+import com.example.gaofei.myapplication.utils.LogUtils;
+
+/**
+ * Created by gaofei on 31/10/2017.
+ */
+
+public class Application2 {
+    public void git(){
+        LogUtils.d("1");
+    }
+}
