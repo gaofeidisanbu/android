e79d7c3c52f4898be02fc30745fc07dc933ad57e
gaofei <13071135067@163.com>
2018-01-14 00:47:04 +0800

    焦点

--- MyApplication/app/src/main/java/com/gaofei/app/act/EditTextAct.java
+++ MyApplication/app/src/main/java/com/gaofei/app/act/EditTextAct.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import com.gaofei.app.R;
 import com.gaofei.app.databinding.ActEditTextBinding;
 import com.gaofei.library.base.BaseAct;
+import com.gaofei.library.utils.LogUtils;
 import com.gaofei.library.utils.ToastManager;
 
 /**
@@ -20,16 +21,41 @@ public class EditTextAct extends BaseAct {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.act_edit_text);
         mBinding.button1.setOnClickListener( v -> {
-            mBinding.edit.setOnClickListener( e -> {
-                ToastManager.show("button1 onclick");
-            });
-            mBinding.edit.setFocusable(false);
+            LogUtils.d("editText "
+                    +"\n e focus = "+mBinding.edit.isFocusable()
+                    +"\n e isFocusableInTouchMode = "+mBinding.edit.isFocusableInTouchMode()
+                    +"\n t focus = "+mBinding.text.isFocusable()
+                    +"\n t isFocusableInTouchMode = "+mBinding.text.isFocusableInTouchMode()
+                    +"\n b1 focus = "+mBinding.button1.isFocusable()
+                    +"\n b1 isFocusableInTouchMode = "+mBinding.button1.isFocusableInTouchMode()
+                    +"\n b2 focus = "+mBinding.button2.isFocusable()
+                    +"\n b2 isFocusableInTouchMode = "+mBinding.button2.isFocusableInTouchMode()
+                    +"\n e2 focus = "+mBinding.edit2.isFocusable()
+                    +"\n e2 isFocusableInTouchMode = "+mBinding.edit2.isFocusableInTouchMode());
+            ToastManager.show("button1");
         });
         mBinding.button2.setOnClickListener( v -> {
-            mBinding.edit.setFocusable(true);
-//            mBinding.edit.setFocusableInTouchMode(true);
-            mBinding.edit.setOnClickListener(null);
-            ToastManager.show(mBinding.edit.isFocused()+"requestFocus =  "+mBinding.edit.requestFocus());
+            mBinding.button1.setFocusable(false);
+            ToastManager.show("button2");
+        });
+        mBinding.button3.setOnClickListener( v -> {
+            mBinding.button1.setFocusableInTouchMode(true);
+            mBinding.button2.setFocusableInTouchMode(true);
+            ToastManager.show("button3");
+        });
+
+        mBinding.button4.setOnClickListener( v -> {
+            mBinding.button1.requestFocus();
+            LogUtils.d("edit isFocused = "
+                    +mBinding.edit.isFocused()
+                    +" edit2 isFocused = "
+                    +mBinding.edit2.isFocused()
+                    +" text isFocused = "
+                    +mBinding.text.isFocused()
+                    +" button1 isFocused = "
+                    +mBinding.button1.isFocused()
+                    +" button2 isFocused = "
+                    +mBinding.button2.isFocused());
         });
 
     }
