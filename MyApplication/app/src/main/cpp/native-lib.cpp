346e4c5f9269b627cfa84894adac6de5daecffdc
gaofei <gf>
2020-01-19 16:57:42 +0800

    ndk

--- MyApplication/app/src/main/cpp/native-lib.cpp
+++ MyApplication/app/src/main/cpp/native-lib.cpp
@@ -7,11 +7,12 @@
 
 extern "C" JNIEXPORT jstring
 
-JAVA_com_gaofei_app_act_TestAct_stringFromJNI(
+Java_com_gaofei_app_act_TestAct_stringFromJNI(
         JNIEnv *env,
         jobject ) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
 
 
--- MyApplication/app/src/main/java/com/gaofei/app/act/TestAct.java
+++ MyApplication/app/src/main/java/com/gaofei/app/act/TestAct.java
@@ -1,26 +1,11 @@
 package com.gaofei.app.act;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.widget.TextView;
 
 import com.gaofei.app.R;
-import com.gaofei.app.http.OkhttpUtils;
-import com.gaofei.app.plugin.EncryptUtil;
-import com.gaofei.library.TestKotlin;
 import com.gaofei.library.base.BaseAct;
-import com.gaofei.library.utils.LogUtils;
-
-import java.util.concurrent.Callable;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by gaofei on 2017/6/29.
