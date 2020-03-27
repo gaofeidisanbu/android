cf5c5c6b95561fe154f219b2d70418053afee21b
gaofei <gf>
2020-03-27 10:54:35 +0800

    android4

--- MyApplication/android4/src/main/java/com/example/android4/MainActivity.kt
+++ MyApplication/android4/src/main/java/com/example/android4/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.android4
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
