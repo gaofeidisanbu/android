f76088e817c28ec6591553460e7c2cec8e668083
gaofei <gf>
2020-05-20 10:50:49 +0800

    动态代理

--- MyApplication/app/src/main/java/com/gaofei/app/act/TestAct.java
+++ MyApplication/app/src/main/java/com/gaofei/app/act/TestAct.java
@@ -4,13 +4,16 @@ import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
 
 import com.gaofei.app.R;
 import com.gaofei.app.fra.BaseDialogFragment;
+import com.gaofei.library.ProjectApplication;
 import com.gaofei.library.base.BaseAct;
+import com.gaofei.library.utils.FileUtils;
 import com.gaofei.library.utils.LogUtils;
 
 import io.reactivex.Observable;
@@ -44,11 +47,33 @@ public class TestAct extends BaseAct {
             @Override
             public void onClick(View v) {
 //                new BaseDialogFragment().show(getSupportFragmentManager(), "");
-                Intent intent = new Intent("android.intent.action_Broadcast_Receiver_Test");
-                intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-                intent.setPackage("com.gaofei.app");
-                sendBroadcast(intent);
+//                Intent intent = new Intent("android.intent.action_Broadcast_Receiver_Test");
+//                intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+//                intent.setPackage("com.gaofei.app");
+//                sendBroadcast(intent);
                 text.setText("");
+                int i = 0;
+
+                for (i = 0; i < 1000; i++) {
+                    new Thread(new RunnableTest()).start();
+                }
+                for (i = 0; i < 20000; i++) {
+                    for (int j = 0; j < 20000; j++) {
+//                        try {
+//                            Thread.sleep(3);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        int c = i +j;
+//                        new Handler().postDelayed(new Runnable() {
+//                            @Override
+//                            public void run() {
+//
+//                                text.setText("i = "+" j = "+c);
+//                            }
+//                        }, 1000);
+                    }
+                }
             }
         });
     }
@@ -56,4 +81,29 @@ public class TestAct extends BaseAct {
 
     public native String stringFromJNI();
 
+    static class RunnableTest implements Runnable {
+        @Override
+        public void run() {
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < 10000000; i++) {
+                new Object();
+                new Object();
+                new Object();
+                new Object();
+                new Object();
+                for (j = 0; j < 10000000; j++) {
+                    new Object();
+                    new Object();
+                    new Object();
+                    new Object();
+                    new Object();
+                    FileUtils.save(ProjectApplication.getContext(), "aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                }
+            }
+        }
+    }
+
 }
+
+
