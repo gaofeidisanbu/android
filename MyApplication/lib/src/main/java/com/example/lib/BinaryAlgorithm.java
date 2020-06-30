463774d806328436603b02df5eb961a40200ef8e
gf <gaofei1207@gmail.com>
2020-06-30 16:38:53 +0800

    二进制相关算法

--- MyApplication/lib/src/main/java/com/example/lib/BinaryAlgorithm.java
+++ MyApplication/lib/src/main/java/com/example/lib/BinaryAlgorithm.java
@@ -3,12 +3,12 @@ package com.example.lib;
 public class BinaryAlgorithm {
 
     public static void main(String[] args) {
-        System.out.println(add(-5, 6));
+        System.out.println(add(-5, -6));
     }
 
     private static int add(int a, int b) {
         int sum = a;
-        while (b > 0) {
+        while (b != 0) {
             sum = a ^ b;
             b = ((a & b) << 1);
             a = sum;
